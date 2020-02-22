42c730d4db42a78f1394a3f190b8199f4789423c
Nopri <if317011@students.del.ac.id>
2020-02-22 17:06:54 +0700

    Changes file to php

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
+<?php
+echo "Hello World";
+?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Selamat Datang di Website Biodata</title>
+    <h2>Haiiiii</h2>
 </head>
 <body>
     
